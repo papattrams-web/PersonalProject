d30edd22ae7d0ba9a26440e074e968ff71f0059f
papattrams-web <papattrams@gmail.com>
2025-12-14 18:26:00 +0000

    Fixed filling multple empty squares, fixed play again after winner has been announced

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -1,23 +1,39 @@
-const characters= ["X", "O"]
 const board= document.querySelector(".board")
-
 const play= document.querySelector("#play")
-play.addEventListener("click", clickPlay)
-
 const characterTile= document.querySelector(".characters")
 
+const characters= []
 const boardArray= []
+
+let inserted= null
+let selectedTile= null
+let winner= null
 
 const length= 3
 
-
-let selectedTile= null
-
-let winner= null
-
 function startGame(){
+    play.removeEventListener("click", startGame)
+    play.addEventListener("click", clickPlay)
+    play.innerText= "Play"
+
+    board.innerHTML = ''
+    boardArray.length = 0
+    characterTile.innerHTML = ''
+
     createBoard()
     populateChar()
+
+    inserted = null
+    selectedTile = null
+    winner = null
+
+    // currentPlayer = null 
+    // opponent = null
+    // gameActive = true
+    
+    // play.innerText= "SELECT PLAYER" // CHANGE: Button text updated to guide user
+    // play.removeEventListener("click", playAgain) // Ensure old listener is removed
+    // play.addEventListener("click", clickPlay)
 }
 
 startGame()
@@ -36,6 +52,10 @@ function createBoard(){
 }
 
 function populateChar(){
+    characters.length= 0
+    characters.push("X")
+    characters.push("O")
+
     for(let i=0; i<characters.length; i++){
         const tile= document.createElement("div")
 
@@ -51,42 +71,53 @@ function populateChar(){
 }
 
 function selected(){
-    if(selectedTile==null){
-        selectedTile= this
-        selectedTile.classList.toggle("selected")
-    }else{
-
+    if(selectedTile!=null){
         selectedTile.classList.remove("selected")
-        selectedTile= null
     }
+    
+    selectedTile= this
+    selectedTile.classList.add("selected")
 }
 
 function insertChar(){
-    if(!selectedTile)return
+    if(!selectedTile||this.classList.contains("immutable"))return
+    
+    if(inserted!=null){
+        inserted.innerText= null
+        this.innerText= selectedTile.innerText
+    }
+    
     this.innerText= selectedTile.innerText
+    inserted= this
 }
 
 function clickPlay(){
     for(let i=0; i<boardArray.length; i++){
         if(boardArray[i].innerText && !boardArray[i].classList.contains("immutable")){
             boardArray[i].classList.add("immutable")
+            inserted= null
         }
     }
 
     checkWin()
 }
 
+play.addEventListener("click", clickPlay)
+
 function checkWin(){
     if(rowCheck() || colCheck()||diagonalCheck()){
-        alert(`WINNER: ${winner}`)
         board.classList.add("immutable")
+        alert(`WINNER: ${winner}`)
+
+        play.innerText= "Play Again"
+        play.addEventListener("click", startGame)
     }
 }
 
 function colCheck(){
     for(let i=0; i<3; i++){
         //check if element is not empty and elements on the same column are equal
-        if(boardArray[i].innerText && boardArray[i].innerText==boardArray[i+3].innerText && boardArray[i].innerText==boardArray[i+6].innerText){
+        if(boardArray[i].innerText && boardArray[i].innerText && boardArray[i].innerText==boardArray[i+3].innerText && boardArray[i].innerText==boardArray[i+6].innerText){
             if(boardArray[i]==="X"){
                 winner= "Y"
             }else{
@@ -101,12 +132,12 @@ function colCheck(){
 }
 
 function rowCheck(){
-    for(let i=0; i<boardArray.length-2; i++){
+    for(let i=0; i<boardArray.length-2; i+=3){
         //check if element is not empty and elements on the same row are equal
-        if(boardArray[i].innerText && i%3==0 && (boardArray[i].innerText==boardArray[i+1].innerText && boardArray[i].innerText==boardArray[i+2].innerText)){
+        if(boardArray[i].innerText && boardArray[i].innerText==boardArray[i+1].innerText && boardArray[i].innerText==boardArray[i+2].innerText){
             
             if(boardArray[i]==="X"){
-                winner= "Y"
+                winner= "O"
             }else{
                 winner= "X"
             }
@@ -119,5 +150,14 @@ function rowCheck(){
 }
 
 function diagonalCheck(){
-    return boardArray[0]==boardArray[4] && boardArray[0]==boardArray[8] || boardArray[2]==boardArray[4] && boardArray[2]==boardArray[6]
+    if (boardArray[0].innerText && boardArray[0].innerText==boardArray[4].innerText && boardArray[0].innerText==boardArray[8].innerText || boardArray[2].innerText && boardArray[2].innerText==boardArray[4].innerText && boardArray[2].innerText==boardArray[6].innerText){
+        if(winner= boardArray[0].innerText==="X"){
+                winner= "O"
+            }else{
+                winner= "X"
+            }
+
+        return true
+    }
+    return false
 }
