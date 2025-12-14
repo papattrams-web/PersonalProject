14c86708cf7babb75e994c3fe18ffe9313213609
papattrams-web <papattrams@gmail.com>
2025-12-14 16:58:18 +0000

    Fixed minor game logic

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -1,11 +1,19 @@
 const characters= ["X", "O"]
 const board= document.querySelector(".board")
+
+const play= document.querySelector("#play")
+play.addEventListener("click", clickPlay)
+
 const characterTile= document.querySelector(".characters")
 
 const boardArray= []
 
 const length= 3
+
+
 let selectedTile= null
+
+let winner= null
 
 function startGame(){
     createBoard()
@@ -22,7 +30,7 @@ function createBoard(){
         tile.addEventListener("click", insertChar)
 
         board.appendChild(tile)
-        boardArray.push()
+        boardArray.push(tile)
 
     }
 }
@@ -45,48 +53,71 @@ function populateChar(){
 function selected(){
     if(selectedTile==null){
         selectedTile= this
+        selectedTile.classList.toggle("selected")
     }else{
-        
+
+        selectedTile.classList.remove("selected")
+        selectedTile= null
     }
 }
 
 function insertChar(){
-    this.innerText= selectedTile
+    if(!selectedTile)return
+    this.innerText= selectedTile.innerText
 }
 
-function play(){
+function clickPlay(){
     for(let i=0; i<boardArray.length; i++){
         if(boardArray[i].innerText && !boardArray[i].classList.contains("immutable")){
-            boardArray.classList.add("immutable")
+            boardArray[i].classList.add("immutable")
         }
     }
 
-    if(checkWin()){
-
-    }else{
-        
-    }
+    checkWin()
 }
 
 function checkWin(){
-    rowCheck()
-    colCheck()
+    if(rowCheck() || colCheck()||diagonalCheck()){
+        alert(`WINNER: ${winner}`)
+        board.classList.add("immutable")
+    }
 }
 
 function colCheck(){
     for(let i=0; i<3; i++){
         //check if element is not empty and elements on the same column are equal
-        if(boardArray[i].innerText && boardArray[i].innerText==boardArray[i+3].innerText==boardArray[i+6].innerText){
+        if(boardArray[i].innerText && boardArray[i].innerText==boardArray[i+3].innerText && boardArray[i].innerText==boardArray[i+6].innerText){
+            if(boardArray[i]==="X"){
+                winner= "Y"
+            }else{
+                winner= "X"
+            }
+
             return true
         }
     }
+
+    return false
 }
 
-function colCheck(){
-    for(let i=0; i<boardArray.length; i++){
+function rowCheck(){
+    for(let i=0; i<boardArray.length-2; i++){
         //check if element is not empty and elements on the same row are equal
-        if(boardArray[i].innerText && i%3==0 && (boardArray[i].innerText==boardArray[i+1].innerText==boardArray[i+2].innerText)){
+        if(boardArray[i].innerText && i%3==0 && (boardArray[i].innerText==boardArray[i+1].innerText && boardArray[i].innerText==boardArray[i+2].innerText)){
+            
+            if(boardArray[i]==="X"){
+                winner= "Y"
+            }else{
+                winner= "X"
+            }
+ 
             return true
         }
     }
+
+    return false
+}
+
+function diagonalCheck(){
+    return boardArray[0]==boardArray[4] && boardArray[0]==boardArray[8] || boardArray[2]==boardArray[4] && boardArray[2]==boardArray[6]
 }
